e233318dd9f148042be4c5fba23585a0d7264eff
Andrew Platosh <aplatosh6982@student.pps.net>
2025-11-21 08:27:04 +0000

    Basic Bot Working

--- players/andys_bot.py
+++ players/andys_bot.py
@@ -19,7 +19,7 @@ class andys_bot(PokerBotAPI):
         card_value = 0
         
         #define hand strengths through dictionary
-        Strengths = {
+        strengths = {
             "pair": 25,
             "high_card": 5,
             "ace_high": 20,
@@ -27,12 +27,13 @@ class andys_bot(PokerBotAPI):
             "consecutive_numbers": 50,
             "same_suit": 25}
 
-        isPair = (card1.Rank.value == card2.Rank.value)
-        isHighCard = (card1.Rank.value >= Rank.JACK.value or card2.Rank.value >= Rank.JACK.value)
-        isAceHigh = (card1.Rank == Rank.ACE or card2.Rank == Rank.ACE)
-        isPocketAces = (card1.Rank == Rank.ACE and card2.Rank == Rank.ACE)
-        isConsecutive = (abs(card1.Rank.value - card2.Rank.value) == 1)
-        isSameSuit = (card1.Suit == card2.Suit)
+        isPair = (card1.rank.value == card2.rank.value)
+        isHighCard = (card1.rank.value >= Rank.JACK.value or card2.rank.value >= Rank.JACK.value)
+        isAceHigh = (card1.rank == Rank.ACE or card2.rank == Rank.ACE)
+        isPocketAces = (card1.rank == Rank.ACE and card2.rank == Rank.ACE)
+        isConsecutive = (abs(card1.rank.value - card2.rank.value) == 1)
+        isSameSuit = (card1.suit == card2.suit)
+
 
         # evaluate hand strength
         card_value += strengths["pair"] * isPair
@@ -47,12 +48,12 @@ class andys_bot(PokerBotAPI):
         if card_value >= 300:
             intendedAction = PlayerAction.ALL_IN
             intendedAmount = max_bet
-        elif card_value >= 100 and cardValue <= 300:
+        elif card_value >= 100:
             intendedAction = PlayerAction.RAISE
-            if max_bet >= cardValue:
+            if max_bet >= card_value:
                 intendedAmount = max_bet
             else:
-                intendedAmount = cardValue
+                intendedAmount = card_value
         else:
             intendedAction = PlayerAction.CHECK
             intendedAmount = 0
@@ -70,5 +71,7 @@ class andys_bot(PokerBotAPI):
             return PlayerAction.ALL_IN, max_bet
         else:
             return PlayerAction.FOLD, 0
+        
+
     def hand_complete(self, game_state, hand_result):
         pass
